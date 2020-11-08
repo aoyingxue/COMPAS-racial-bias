c6ed73ee932a7f06bc76ff1b647bf32bcdbaaaff
Yuki Ao <aoyingxue@gmail.com>
2020-11-07 19:30:48 -0600

    update descriptive part

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os, urllib
+import matplotlib.pyplot as plt
 
 # URLError workaround
 import ssl
@@ -47,16 +48,56 @@ def get_file_content_as_string(path):
         data = file.read()
     return data
 
+# read data from cache
+@st.cache
+def load_data(nrows,dataset_name):
+    if dataset_name=="COMPAS Raw Scores":
+        path="data/compas-scores-raw.csv"
+    elif dataset_name=="Compass Score after parsing and cleaning":
+        path="data/cox-violent-parsed.csv"
+    data = pd.read_csv(path, nrows=nrows)
+    # preprocess goes from here
+    return data
+
 def descriptive_analysis():
     st.title("Descriptive Analysis")
+    dataset_name = st.sidebar.selectbox(
+        "Select Dataset", ("COMPAS Raw Scores", "Compass Score after parsing and cleaning"))
+    data = load_data(10000,dataset_name)
 
+    # checkbox
+    if st.checkbox('View Chosen Data: %s' %dataset_name):
+        st.subheader(dataset_name)
+        st.dataframe(data)
+
+
+    # code box
+    # with st.echo():
+    #     st.write(test_data.head())
+
+    # slider
+    # st.slider()
+
+    with st.beta_container():
+        var = st.selectbox("Choose a Variable", data.columns)
+        st.write('## Histogram of %s' % var)
+        # histogram
+        fig = plt.figure()
+        plt.hist(data[var])
+        plt.xticks(rotation=270)
+        st.pyplot(plt)
+    # dropdown to select the variable
+
+## Model Page
 def run_the_model():
     st.title("Regression Model")
 
+
+## Prediction Page
 def run_the_prediction():
     st.title("Score Prediction")
 
 
-# Execute
+# Execute the main function
 if __name__ == "__main__":
     main()
